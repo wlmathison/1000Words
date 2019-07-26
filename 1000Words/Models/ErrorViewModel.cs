2bd01de78bd4026ff9d38af4d90cf1bdf4149632
Billy Mathison <wlmathison@gmail.com>
2019-07-26 11:26:11 -0500

    Initial commit

---
+++ 1000Words/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _1000Words.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
