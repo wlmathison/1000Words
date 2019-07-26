e7488aea9c2a3bf0cb99d9ef2f31ae6627871069
Billy Mathison <wlmathison@gmail.com>
2019-07-26 14:23:10 -0500

    Create models for application user, photo, description, and album. Adding json file with google key and including in gitignore.

---
+++ 1000Words/Models/Album.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _1000Words.Models
+{
+    public class Album
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public int UserId { get; set; }
+
+        public ApplicationUser User { get; set; }
+    }
+}
