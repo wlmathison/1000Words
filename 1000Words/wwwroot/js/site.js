5590ed59f230e36f09b2044952d81454195c7646
Billy Mathison <wlmathison@gmail.com>
2019-08-07 15:17:31 -0500

    Styling navbar

--- 1000Words/wwwroot/js/site.js
+++ 1000Words/wwwroot/js/site.js
@@ -1,4 +1,4 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+﻿z// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
 // webkitURL is deprecated but nevertheless
